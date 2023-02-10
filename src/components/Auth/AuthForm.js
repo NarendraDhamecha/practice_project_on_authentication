1d3ea570ab844bd2c0d02a254d615b16dd59b7bd
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-02-10 17:55:26 +0530

    signup_and_authentication

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,79 @@
+import { useRef, useState } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    setIsLoading(true);
+    try {
+      if (isLogin) {
+      } else {
+        const res = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwX1LEI0sFtNSiRy9x1fnxL52NZXOX-s0",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: enteredEmail,
+              password: enteredPassword,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "content-type": "application/json",
+            },
+          }
+        );
+       setIsLoading(false);
+       if(res.ok){
+        
+       }else{
+        const data = await res.json();
+        throw new Error(data.error.message);
+       }
+       
+      }
+    } catch (e) {
+       alert(e);
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailRef} type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input ref={passwordRef} type="password" id="password" required />
+        </div>
+        <div className={classes.actions}>
+          {isLoading? <p>Sending request...</p> : <button>{isLogin ? "Login" : "Create Account"}</button>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
